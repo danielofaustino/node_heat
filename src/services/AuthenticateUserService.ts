c12072122e5bf3710099555c08ac5a0614dc29fb
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-10-20 22:38:54 -0300

    User Authentication

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
 // Receber ConvolverNode(string) *
 // Recuperar o access_token no github *
 // Recuperar infos do user no github *
-// VErificar se o usuario existe no DB
+// VErificar se o usuario existe no DB *
 // ---- SIM = gera um token
 // ---- NAO = Cria no DB, gera um token
 // Retornar o token com as infos do user
@@ -42,7 +44,40 @@ class AuthenticateUserService {
       }
     );
 
-    return response.data;
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id,
+      },
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name,
+        },
+      });
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      }
+    );
+    return { token, user };
   }
 }
 
